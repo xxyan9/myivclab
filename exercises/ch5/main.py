730ed4be32c2aced007af14416bdcee3d3ac09d1
xxyan9 <jp2020213183@qmul.ac.uk>
2025-07-16 02:15:39 +0200

    add test codes for Ch5-1

--- exercises/ch5/main.py
+++ exercises/ch5/main.py
@@ -4,9 +4,11 @@ import numpy as np
 from ivclab.utils import imread, calc_psnr
 import matplotlib.pyplot as plt
 from ivclab.signal import rgb2ycbcr, ycbcr2rgb
+import deblock
 
 lena_small = imread('../data/lena_small.tif')
 
+'''
 # 1. chapter 3: Intra Coding
 images = []
 for i in range(20, 40 + 1):
@@ -31,13 +33,14 @@ for q_scale in [0.15, 0.3, 0.7, 1.0, 1.5, 3, 5, 7, 10]:
         bpp = bitrate / (H * W)
         image_psnrs.append(psnr)
         image_bpps.append(bpp)
-        print(f"Frame:{i} PSNR: {psnr:.2f} dB bpp: {bpp:.2f}")
+
     psnr = np.mean(image_psnrs)
     bpp = np.mean(image_bpps)
     all_bpps.append(bpp)
     all_PSNRs.append(psnr)
     print(f"Q-Scale {q_scale}, PSNR: {psnr:.2f} dB, bpp: {bpp:.2f}")
 
+print('-'*12)
 ch3_bpps = np.array(all_bpps)
 ch3_psnrs = np.array(all_PSNRs)
 
@@ -50,6 +53,7 @@ all_bpps = list()
 all_psnrs = list()
 
 for q_scale in [0.07, 0.2, 0.4, 0.8, 1.0, 1.5, 2, 3, 4, 4.5]:
+# for q_scale in [1]:
     video_codec = VideoCodec(quantization_scale=q_scale)
     video_codec.intra_codec.train_huffman_from_image(lena_small, is_source_rgb=True)
     bpps = list()
@@ -69,20 +73,56 @@ for q_scale in [0.07, 0.2, 0.4, 0.8, 1.0, 1.5, 2, 3, 4, 4.5]:
 
 ch4_bpps = np.array(all_bpps)
 ch4_psnrs = np.array(all_psnrs)
+'''
 
-np.save('../data/ch4_bpps.npy', ch4_bpps)
-np.save('../data/ch4_psnrs.npy', ch4_psnrs)
+# 3. Chapter 5-1: In-loop Deblocking Filter
+images = []
+for i in range(20, 40 + 1):
+    images.append(imread(f'../data/foreman20_40_RGB/foreman00{i}.bmp'))
+
+all_bpps = []
+all_psnrs = []
+
+for q_scale in [0.07, 0.2, 0.4, 0.8, 1.0, 1.5, 2, 3, 4, 4.5]:
+# for q_scale in [1.0]:
+    video_codec = VideoCodec(quantization_scale=q_scale)
+    video_codec.intra_codec.train_huffman_from_image(lena_small, is_source_rgb=True)
+
+    bpps = []
+    psnrs = []
+
+    for frame_num, image in enumerate(images):
+        reconstructed_img, bitstream, bitsize = video_codec.encode_decode(image, frame_num=frame_num)
+        bpp = bitsize / (image.size / 3)
+        psnr = calc_psnr(image, reconstructed_img)
+        print(f"Frame:{frame_num} PSNR: {psnr:.2f} dB bpp: {bpp:.2f}")
+        bpps.append(bpp)
+        psnrs.append(psnr)
+
+    all_bpps.append(np.mean(bpps))
+    all_psnrs.append(np.mean(psnrs))
+    print(f"Q-Scale {q_scale}, PSNR: {psnr:.2f} dB, bpp: {bpp:.2f}")
+    print("-" * 12)
+
+ch5_1_bpps = np.array(all_bpps)
+ch5_1_psnrs = np.array(all_psnrs)
+
+np.save('../data/ch5_1_bpps.npy', ch5_1_bpps)
+np.save('../data/ch5_1_psnrs.npy', ch5_1_psnrs)
 
 ch3_bpps = np.load('../data/ch3_bpps.npy')
 ch3_psnrs = np.load('../data/ch3_psnrs.npy')
 ch4_bpps = np.load('../data/ch4_bpps.npy')
 ch4_psnrs = np.load('../data/ch4_psnrs.npy')
+ch5_1_bpps = np.load('../data/ch5_1_bpps.npy')
+ch5_1_psnrs = np.load('../data/ch5_1_psnrs.npy')
 
 plt.figure()
 plt.xlabel('Bitrate (bpp)')
 plt.ylabel('PSNR [dB]')
 plt.title('Rate-Distortion Curve')
+plt.plot(ch3_bpps, ch3_psnrs, linestyle='--', marker='.', color='orange', label='Image Codec Solution')
 plt.plot(ch4_bpps, ch4_psnrs, linestyle='--', marker='s', color='blue', label='Video Codec Solution')
-plt.plot(ch3_bpps, ch3_psnrs, linestyle='--', marker='s', color='orange', label='Image Codec Solution')
+plt.plot(ch5_1_bpps, ch5_1_psnrs, linestyle='--', marker='x', color='purple', label='Optimization 1')
 plt.legend()
 plt.show()
