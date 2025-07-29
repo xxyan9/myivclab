85e44d6fc2af85a45697707af1f4853b7ba41d48
xxyan9 <jp2020213183@qmul.ac.uk>
2025-07-29 11:31:40 +0200

    Change some notes

--- exercises/ch5/halfpel.py
+++ exercises/ch5/halfpel.py
@@ -1,10 +1,9 @@
 import numpy as np
-from ivclab.entropy import ZeroRunCoder
 from ivclab.image import IntraCodec
 from ivclab.entropy import HuffmanCoder, stats_marg
 from ivclab.signal import rgb2ycbcr, ycbcr2rgb
 from ivclab.video import MotionCompensator
-from ivclab.utils import calc_psnr
+from ivclab.utils import imread, calc_psnr
 import matplotlib.pyplot as plt
 
 
@@ -282,46 +281,8 @@ class VideoCodec:
         return self.decoder_recon, bitstream, bitsize
 
 if __name__ == "__main__":
-    from ivclab.image import IntraCodec
-    import numpy as np
-    from ivclab.utils import imread, calc_psnr
-    import matplotlib.pyplot as plt
-    from ivclab.signal import rgb2ycbcr, ycbcr2rgb
-
     lena_small = imread('../data/lena_small.tif')
-    '''
-    # 2. Chapter 4: Video Coding
-    images = []
-    for i in range(20, 40 + 1):
-        images.append(imread(f'../data/foreman20_40_RGB/foreman00{i}.bmp'))
 
-    all_bpps = list()
-    all_psnrs = list()
-
-    for q_scale in [0.07, 0.2, 0.4, 0.8, 1.0, 1.5, 2, 3, 4, 4.5]:
-        video_codec = VideoCodec(quantization_scale=q_scale, use_half_pel=False)
-        video_codec.intra_codec.train_huffman_from_image(lena_small, is_source_rgb=True)
-        bpps = list()
-        psnrs = list()
-        for frame_num, image in enumerate(images):
-            reconstructed_image, bitstream, bitsize = video_codec.encode_decode(image, frame_num=frame_num)
-            bpp = bitsize / (image.size / 3)
-            psnr = calc_psnr(image, reconstructed_image)
-            print(f"Frame:{frame_num} PSNR: {psnr:.2f} dB bpp: {bpp:.2f}")
-            bpps.append(bpp)
-            psnrs.append(psnr)
-
-        all_bpps.append(np.mean(bpps))
-        all_psnrs.append(np.mean(psnrs))
-        print(f"Q-Scale {q_scale}, Average PSNR: {np.mean(psnrs):.2f} dB Average bpp: {np.mean(bpps):.2f}")
-        print('-' * 12)
-
-    ch4_bpps = np.array(all_bpps)
-    ch4_psnrs = np.array(all_psnrs)
-
-    np.save('../data/ch4_bpps.npy', ch4_bpps)
-    np.save('../data/ch4_psnrs.npy', ch4_psnrs)
-    '''
     images = []
     for i in range(20, 40 + 1):
         images.append(imread(f'../data/foreman20_40_RGB/foreman00{i}.bmp'))
@@ -344,11 +305,13 @@ if __name__ == "__main__":
         all_bpps.append(np.mean(bpps))
         all_PSNRs.append(np.mean(psnrs))
         print(f"Q-Scale {q_scale}, Average PSNR: {np.mean(psnrs):.2f} dB Average bpp: {np.mean(bpps):.2f}")
+        print('-' * 12)
+
     ch5_halfpel_bpps = np.array(all_bpps)
     ch5_halfpel_psnrs = np.array(all_PSNRs)
-
     np.save('../data/ch5_halfpel_bpps.npy', ch5_halfpel_bpps)
     np.save('../data/ch5_halfpel_psnrs.npy', ch5_halfpel_psnrs)
+
     ch4_bpps = np.load('../data/ch4_bpps.npy')
     ch4_psnrs = np.load('../data/ch4_psnrs.npy')
     ch5_halfpel_bpps = np.load('../data/ch5_halfpel_bpps.npy')
